1965f117ae599d36bc7319b4728ce9e64d155c97
Ghazanfar Ali <160585421+GhazanfarAli9@users.noreply.github.com>
2024-05-09 18:11:20 +0500

    push to calculator to github

---
+++ main.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+let calculator = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "Enter the number here ",
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter the second number",
+    },
+    {
+        name: "Operations",
+        type: "list",
+        choices: ["Addition", "Subtraction", "Multiplication", "division"],
+    },
+]);
+//conitional statement!
+if (calculator.Operations === "Addition") {
+    console.log(calculator.num1 + calculator.num2);
+}
+else if (calculator.Operations === "Subraction") {
+    console.log(calculator.num1 - calculator.num2);
+}
+else if (calculator.Operations === "Multiplication") {
+    console.log(calculator.num1 * calculator.num2);
+}
+else if (calculator.Operations === "division") {
+    console.log(calculator.num1 / calculator.num2);
+}
